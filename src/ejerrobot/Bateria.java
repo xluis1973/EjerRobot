1f8787e47a506123a1f873ba934ae83e746c1548
Luis Javier Mercado <50953075+xluis1973@users.noreply.github.com>
2023-08-11 17:19:56 -0300

    Initial commit

---
+++ src/ejerrobot/Bateria.java
@@ -0,0 +1,32 @@
+
+package ejerrobot;
+
+public class Bateria {
+    private String tipoBateria;
+    private int carga;
+    
+    public Bateria(){
+    
+        this.carga=1000;
+    }
+
+    public String getTipoBateria() {
+        return tipoBateria;
+    }
+
+    public void setTipoBateria(String tipoBateria) {
+        this.tipoBateria = tipoBateria;
+    }
+
+    public int getCarga() {
+        return carga;
+    }
+
+    public void setCarga(int carga) {
+        this.carga = carga;
+    }
+
+   
+    
+    
+}
