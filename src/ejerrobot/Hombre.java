f345c6487919fd83bf69551be84f7862524951ec
Alberto <btamengual48@gmail.com>
2023-08-11 18:19:20 -0300

    create class Hombre
    Add class Hombre

---
+++ src/ejerrobot/Hombre.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerrobot;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Hombre {
+    private Robot robot;
+    private String nombre;
+
+    public Hombre(Robot robot, String nombre) {
+        this.robot = robot;
+        this.nombre = nombre;
+    }
+    
+    public void JugarConRobot(Robot robot){
+      robot.robotAvanzar(1);
+      robot.dormir();
+      robot.despertar();
+      robot.robotRetroceder(0);
+    }
+}
